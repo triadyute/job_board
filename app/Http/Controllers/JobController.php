3461b6a1ec0c70c9a16060a98df8a4005eed83df
triadyute <fabianc_henry@yahoo.com>
2019-11-18 11:23:23 -0500

    second commit

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -18,46 +18,116 @@ class JobController extends Controller
         return view('welcome', compact('jobs'));
     }
 
-    public function results(Request $request){
+    public function results(Request $request)
+    {
         $jobs = Job::all();
+
         $requirements = $request->requirements;
-        $results = array();
-        if($requirements == ['apartment', ] ){
+
+        $results_qualified = array();
+        $results_not_qualified = array();
+
+        
+        if((in_array('apartment', $requirements) || in_array('house', $requirements) ) && in_array('property insurance', $requirements))
+        {
             $job = Job::find(1);
-            array_push($results, $job);
-            return $results;
-        }
-        elseif(in_array(['bike', 'driver\'s licence'], $requirements) ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
-        }
-        elseif($requirements == [''] ){
-            return $requirements;
+            array_push($results_qualified, $job);
         }
         else{
-            return $requirements;
+            $job = Job::find(1);
+            array_push($results_not_qualified, $job);
+
         }
 
+        if((in_array('5 door car', $requirements) || in_array('4 door car', $requirements)) && in_array('driver\'s licence', $requirements) && in_array('car insurance', $requirements))
+        {
+            $job = Job::find(2);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(2);
+            array_push($results_not_qualified, $job);
+        } 
 
-        //return $requirements;
+        if(in_array('social security number', $requirements) && in_array('work permit', $requirements))
+        {
+            $job = Job::find(3);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(3);
+            array_push($results_not_qualified, $job);
+        }
+
+        if(in_array('apartment', $requirements) || in_array('flat', $requirements)|| in_array('house', $requirements))
+        {
+            $job = Job::find(4);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(4);
+            array_push($results_not_qualified, $job);
+        }
+
+        if((in_array('driver\'s licence', $requirements)) && (in_array('2 door car', $requirements) || in_array('3 door car', $requirements) || in_array('4 door car', $requirements) || in_array('5 door car', $requirements))){
+            $job = Job::find(5);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(5);
+            array_push($results_not_qualified, $job);
+        }
+
+        if((in_array('scooter', $requirements) || in_array('bike', $requirements))  || (in_array('motorcycle', $requirements) && in_array('driver\'s licence', $requirements) && in_array('motorcycle insurance', $requirements)))  
+        {
+            $job = Job::find(6);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(6);
+            array_push($results_not_qualified, $job);
+        }
+
+        if(in_array('massage qualification certificate', $requirements) && in_array('liability insurance', $requirements))
+        {
+            $job = Job::find(7);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(7);
+            array_push($results_not_qualified, $job);
+        }
+
+        if(in_array('storage place', $requirements) || in_array('garage', $requirements))
+        {
+            $job = Job::find(8);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(8);
+            array_push($results_not_qualified, $job);
+        }
+
+        if(in_array('no requirements', $requirements))
+        {
+            $job = Job::find(9);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(9);
+            array_push($results_not_qualified, $job);
+        }
+        
+        if(in_array('paypal account', $requirements))
+        {
+            $job = Job::find(10);
+            array_push($results_qualified, $job);
+        }
+        else{
+            $job = Job::find(10);
+            array_push($results_not_qualified, $job);
+        }
+        return view('results', compact('results_qualified','results_not_qualified'));
+
     }
-
-
 }
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -47,55 +47,22 @@
 
     <!-- Begin page content -->
     <main role="main" class="container">
-      <h4 class="mt-5">Find Jobs</h4>
+      <h4 class="mt-5">Results</h4>
       <hr>
-      <div class="row">
-        
-        <div class="col-md-6">
-          <div class="form-group">
-            <label for="">What jobs do i qualify for? (select multile)</label>
-            <form method = "POST"action="{{route('search')}}">
-              @csrf
-            <select multiple name="requirements[]" id="" class="form-control mb-2">
-              <option value=""delected disabled>Select requirement</option>
-              <option value="apartment">Apartment</option>
-              <option value="flat">Flat</option>
-              <option value="house">House</option>
-              <option value="storage place">Storage place</option>
-              <option value="property insurance">Property Insurance</option>
-              <option value="2 door car">2 Door Car</option>
-              <option value="3 door car">3 Door Car</option>
-              <option value="4 door car">4 Door Car</option>
-              <option value="5 door car">5 Door Car</option>
-              <option value="driver's licence">Driver's licence</option>
-              <option value="car insurance">Car Insurance</option>
-              <option value="scooter">Scooter</option>
-              <option value="bike">Bike</option>
-              <option value="motorcycle">Motorcycle</option>
-              <option value="motorcycle insurance">Motorcycle insurance</option>
-              <option value="massage qualification certificate">Massage qualification certificate</option>
-              <option value="liability insurance">Liability insurance</option>
-              <option value="social security number">Social Security Number</option>
-              <option value="work permit">Work permit</option>
-              <option value="garage">Garage</option>
-              <option value="paypal account">PayPal account</option>
-              <option value="no requirements">No requirements</option>
-            </select>
-            <button type="submit" class="btn btn-primary">Search</button>
-          </form>
-          </div>
-        </div>
-
-      </div>
-      <h4 class="mt-5">Jobs List</h4>
-      <hr>
-      @if (count($jobs) != 0)
-      @foreach ($jobs as $job)
-      <h5>{{$job->job_title}}</h5>
-      <p><strong>Posted by:</strong> {{$job->company->name}} about {{\Carbon\Carbon::parse($job->created_at)->diffForHumans()}}</p>
-      <p>{{$job->job_description}}</p>
+      <p class="mb-4"><strong>You are qualified for these jobs:</strong></p>
+      @foreach ($results_qualified as $qualified_result )
+          <h4>{{$qualified_result->job_title}}</h4>
+          <p>
+              <strong>Posted by:</strong> {{$qualified_result->company->name}} about {{\Carbon\Carbon::parse($qualified_result->created_at)->diffForHumans()}}<br>
+              {{$qualified_result->job_description}}
+          </p>
       @endforeach
-      @endif
+      <p class="mt-5 mb-4"><strong>You are not qualified for these jobs:</strong></p>
+      @foreach ($results_not_qualified as $not_qualified_result )
+          <h4>{{$not_qualified_result->job_title}}</h4>
+          <p><strong>Posted by:</strong> {{$not_qualified_result->company->name}} about {{\Carbon\Carbon::parse($not_qualified_result->created_at)->diffForHumans()}}<br>
+          {{$not_qualified_result->job_description}}</p>
+      @endforeach
     </main>
 
     <footer class="footer">
